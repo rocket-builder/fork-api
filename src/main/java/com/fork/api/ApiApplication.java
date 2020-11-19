f09175e5b5c7895b5de14a013038797a3a262b16
rocket-builder <bogdan14.228@gmail.com>
2020-11-19 13:42:38 +0300

    add cors filter

--- src/main/java/com/fork/api/ApiApplication.java
+++ src/main/java/com/fork/api/ApiApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class ApiApplication {
